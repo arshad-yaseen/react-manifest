8484c3181462b589537e413766a9eb79675abb02
Glowit Labs <glowitsofware@gmail.com>
2023-01-26 20:43:48 +0530

    error fixed

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const manifest = {
-  change: (manifest_obj, manifest_placeholder) => {
-    const blob = new Blob([manifest_obj], { type: "application/json" });
-    const manifestURL = URL.createObjectURL(blob);
-    document
-      .querySelector(manifest_placeholder)
-      .setAttribute("href", manifestURL);
-  },
-};
+const reactManifest = {
+    update:(manifest_obj, manifest_placeholder) => {
+        const stringManifest = JSON.stringify(manifest_obj);
+        const blob = new Blob([stringManifest], { type: "application/json" });
+        const manifestURL = URL.createObjectURL(blob);
+        document
+          .querySelector(`${manifest_placeholder}`)
+          .setAttribute("href", manifestURL);
+    }
+}
 
-export default manifest;
+export default reactManifest
